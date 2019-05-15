ec65e9e0a0caf1130cc081a8fb1d81f0ca1c7211
Justin Adkins <jadkins1019@gmail.com>
2019-05-15 15:06:38 -0600

    Small style updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
 import './node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './src/components/css/layout.css';
 import './src/components/css/header.css';
 import './src/components/css/home.css';
+
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,15 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginLeft: `auto`, marginRight: `auto`, display: `block`, marginTop: `10px` }}>
-      <Image />
+      <Image filename="treehouse-logo.png"/>
     </div>
     <h1>The Greatest Website Ever</h1>
     <p>Give me all your money.</p>
     <p>You won't regret it.</p>
-    <Linkbox title="about" description="Learn about Deena" id="box-1" />
-    <Linkbox title="services" description="What we do" id="box-2" />
-    <Linkbox title="reviews" description="You're going to like us" id="box-3" />
-    <Linkbox title="contact" description="Get in touch" id="box-4" />
+    <Linkbox title="about" description="Learn about Deena" id="box-1" img="forest-1.jpg" />
+    <Linkbox title="services" description="What we do" id="box-2" img="forest-2.jpg" />
+    <Linkbox title="reviews" description="You're going to like us" id="box-3" img="forest-3.jpg" />
+    <Linkbox title="contact" description="Get in touch" id="box-4" img="forest-4.jpg" />
   </Layout>
 )
 
