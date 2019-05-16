11c43e0b55b1851d70540e24d519e60cc5b06c3f
Justin Adkins <jadkins1019@gmail.com>
2019-05-16 14:26:42 -0600

    Small modifactions

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -20,7 +20,7 @@ const ContactForm = () => (
       </div>
 
       <div className="wrap-input100 rs1-wrap-input100">
-        <span className="label-input100">Email</span>
+        <span className="label-input100">Email Address</span>
         <input className="input100" type="email" name="email" placeholder="Enter your email addess" required />
         <span className="focus-input100"></span>
       </div>
